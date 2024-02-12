a27c3aad2d604ad5699944e07958353fd3d44710
Gichuki Muchir <gichukimuchiri80@gmail.com>
2024-02-12 05:50:12 +0300

    nav-bar update

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// PRELOAD
-// Loading will end after document is loaded
-
-const preloader = document.querySelector("[data-preaload]");
-
-window.addEventListener("load", function () {
- preloader.classList.add ("loaded");
- document.body.classList.add("loaded");
-})
-
 //add event listener on multiple elements
 
 const addEventOnElements = function (elements, eventType, callback) {
@@ -61,64 +51,6 @@ window.addEventListener("scroll", function () {
   backTopBtn.classList.remove("active");
  }
 });
-
-
-// HERO SLIDER
-const heroSlider = document.querySelector("[data-hero-slider]");
-const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
-const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
-const heroSliderNextBtn = document.querySelector("[data-next-btn]");
-
-let currentSlidePos = 0;
-let lastActiveSliderItem = heroSliderItems[0];
-
-const updateSliderPos = function () {
- lastActiveSliderItem.classList.remove("active");
- heroSliderItems[currentSlidePos].classList.add("active");
- lastActiveSliderItem = heroSliderItems[currentSlidePos];
-}
-
-const slideNext = function () {
- if (currentSlidePos >= heroSliderItems.length - 1) {
-  currentSlidePos = 0;
- } else {
-  currentSlidePos++;
- }
-
- updateSliderPos()
-};
-
-heroSliderNextBtn.addEventListener("click", slideNext);
-
-const slidePrev = function () {
- if (currentSlidePos <= 0) {
-  currentSlidePos = heroSliderItems.length - 1;
- } else {
-  currentSlidePos--;
- }
-
- updateSliderPos();
-}
-
-heroSliderPrevBtn.addEventListener("click", slidePrev);
-
-// AUTO SLIDE
-
-let autoSlideInterval;
-
-const autoslide = function () {
- autoSlideInterval = setInterval( function () {
-  slideNext();
- }, 7000);
-}
-
-addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function () {
- clearInterval(autoSlideInterval);
-});
-
-addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", autoslide);
-
-window.addEventListener("load", autoslide);
 
 // PAYMENT CHECKBOX
 
